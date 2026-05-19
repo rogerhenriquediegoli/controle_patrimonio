5aa9304bce9f0ebd026278707c9170d2114047ba
Roger <rogerhenriquediegoli@gmail.com>
2026-05-18 22:16:05 -0300

    feat: Alterações no arquivo Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,29 @@ package src;
 
 import javax.swing.JOptionPane;
 
+import src.dao.MovimentacaoPatrimonioDao;
+import src.dao.PatrimonioDao;
+import src.dao.ResponsavelDao;
+import src.dao.impl.MovimentacaoPatrimonioDaoImpl;
+import src.dao.impl.PatrimonioDaoImpl;
+import src.dao.impl.ResponsavelDaoImpl;
 import src.repository.DbSession;
+import src.service.MovimentacaoPatrimonioService;
+import src.service.PatrimonioService;
+import src.service.ResponsavelService;
+import src.service.impl.MovimentacaoPatrimonioServiceImpl;
+import src.service.impl.PatrimonioServiceImpl;
+import src.service.impl.ResponsavelServiceImpl;
+import src.views.FormResponsavel;
 
 public class Main {
+    private static final ResponsavelDao responsavelDao = new ResponsavelDaoImpl();
+    private static final PatrimonioDao patrimonioDao = new PatrimonioDaoImpl();
+    private static final MovimentacaoPatrimonioDao movimentacaoPatrimonioDao = new MovimentacaoPatrimonioDaoImpl();
+    private static final MovimentacaoPatrimonioService movimentacaoPatrimonioService = new MovimentacaoPatrimonioServiceImpl(movimentacaoPatrimonioDao);
+    private static final PatrimonioService patrimonioService = new PatrimonioServiceImpl(patrimonioDao, movimentacaoPatrimonioService);
+    private static final ResponsavelService responsavelService = new ResponsavelServiceImpl(responsavelDao, patrimonioService, movimentacaoPatrimonioService);
+
     public static void main(String[] args) {
         if (Boolean.FALSE.equals(DbSession.initializeDatabaseEnvironment())) {
             final String mensagem = "Não foi possível conectar ao banco de dados." +
@@ -18,5 +38,9 @@ public class Main {
 
             System.exit(0);
         }
+
+
+
+        new FormResponsavel(responsavelService);
     }
 }
